68a7b5ecdbcdc5df9d54812a0b6f7a05eb46720e
richwingert <richwingert@gmail.com>
2019-07-15 21:09:55 -0400

    Creating initial framework for angular-app

---
+++ ToDoAppAngular/src/app/modules/dashboard/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  template: `
+    <h2>dashboard</h2>
+  `,
+  styles: [`
+    
+  `]
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
